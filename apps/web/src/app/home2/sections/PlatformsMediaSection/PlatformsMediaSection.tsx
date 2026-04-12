23356f45fd077c155a7d9d68ed8fef5bcf8e78e3
XAOVerse <116788787+XAOVerse@users.noreply.github.com>
2026-04-12 20:36:26 +0400

    Reduce Forbes logo by 35% (133x51)

--- apps/web/src/app/home2/sections/PlatformsMediaSection/PlatformsMediaSection.tsx
+++ apps/web/src/app/home2/sections/PlatformsMediaSection/PlatformsMediaSection.tsx
@@ -68,8 +68,8 @@ const JEOverlay = () => (
 
 const ForbesOverlay = () => (
   <div className="flex items-center justify-center w-full h-full" style={{ position: "relative" }}>
-    <img src="/images/forbes-logo-black.svg" alt="Forbes" className={styles.apLogoBlack} style={{ width: 204, height: 79, objectFit: "contain" }} />
-    <img src="/images/forbes-logo-white.svg" alt="Forbes" className={styles.apLogoWhite} style={{ width: 204, height: 79, objectFit: "contain", position: "absolute" }} />
+    <img src="/images/forbes-logo-black.svg" alt="Forbes" className={styles.apLogoBlack} style={{ width: 133, height: 51, objectFit: "contain" }} />
+    <img src="/images/forbes-logo-white.svg" alt="Forbes" className={styles.apLogoWhite} style={{ width: 133, height: 51, objectFit: "contain", position: "absolute" }} />
   </div>
 );
 
