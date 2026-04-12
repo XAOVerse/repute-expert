54a21c410b040b590df6d66c333efa254e940b61
XAOVerse <116788787+XAOVerse@users.noreply.github.com>
2026-04-12 21:37:48 +0400

    Reduce AP logo by 20% (460x130 to 368x104)

--- apps/web/src/app/home2/sections/PlatformsMediaSection/PlatformsMediaSection.tsx
+++ apps/web/src/app/home2/sections/PlatformsMediaSection/PlatformsMediaSection.tsx
@@ -56,8 +56,8 @@ const ArrowUpRight = () => (
 
 const APOverlay = () => (
   <div className="flex items-center justify-center w-full h-full" style={{ position: "relative" }}>
-    <img src="/images/ap-logo.svg" alt="Associated Press" className={styles.apLogoBlack} style={{ width: 460, height: 130, objectFit: "contain" }} />
-    <img src="/images/ap-logo-white.svg" alt="Associated Press" className={styles.apLogoWhite} style={{ width: 460, height: 130, objectFit: "contain", position: "absolute" }} />
+    <img src="/images/ap-logo.svg" alt="Associated Press" className={styles.apLogoBlack} style={{ width: 368, height: 104, objectFit: "contain" }} />
+    <img src="/images/ap-logo-white.svg" alt="Associated Press" className={styles.apLogoWhite} style={{ width: 368, height: 104, objectFit: "contain", position: "absolute" }} />
   </div>
 );
 const BloombergOverlay = () => (
